74692b691e03358098fcd37c2d584cdc50961131
GermanTF <geres439@gmail.com>
2024-10-07 07:52:21 -0500

    Estructura Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
-import { DailyRecipe } from './assets/components/DailyRecipe/DailyRecipe'
+import { DailyRecipe } from './components/DailyRecipe/DailyRecipe'
+import Header  from './Components/Header/Header.jsx';
+import MyLogin from './Components/Header/Login.jsx';
 
 
 function App() {
   return (
     <>
       <DailyRecipe/>
+      <Header/>
+      <MyLogin />
     </>
   )
 }
